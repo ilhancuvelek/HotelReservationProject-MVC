330bacd9b56ba4458cea84da0c67531fe4c79d3a
İlhan Cüvelek <cuvelekilhan@gmail.com>
2023-07-23 12:16:12 +0300

    FluentValidation added

--- Frontend/HotelProject.WebUI/Program.cs
+++ Frontend/HotelProject.WebUI/Program.cs
@@ -1,10 +1,18 @@
+using FluentValidation;
+using FluentValidation.AspNetCore;
 using HotelProject.DataAccessLayer.Concrete;
 using HotelProject.EntityLayer.Concrete;
+using HotelProject.WebUI.Dtos.GuestDto;
+using HotelProject.WebUI.ValidationRules.GuestValidationRules;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews().AddFluentValidation();
+
+//  FluentValidation
+builder.Services.AddTransient<IValidator<CreateGuestDto>, CreateGuestValidator>();
+builder.Services.AddTransient<IValidator<UpdateGuestDto>, UpdateGuestValidator>();
 
 //http client factory nin çalýþmasý için
 builder.Services.AddHttpClient();
@@ -15,6 +23,8 @@ builder.Services.AddAutoMapper(typeof(Program));
 //identity register
 builder.Services.AddDbContext<Context>();
 builder.Services.AddIdentity<AppUser, AppRole>().AddEntityFrameworkStores<Context>();
+
+
 
 var app = builder.Build();
 
